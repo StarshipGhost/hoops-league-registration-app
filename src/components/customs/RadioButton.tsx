6ba4b2a6ff0fb42bda7156d77466a65a93902bb0
Saad Kalyati <saad.kalyati.1@ens.etsmtl.ca>
2026-02-17 21:29:18 -0500

    Indent component's module

--- src/components/customs/RadioButton.tsx
+++ src/components/customs/RadioButton.tsx
@@ -1,11 +1,20 @@
-const CustomRadioButton = ({playerStatus, text, isSelected, style, onChangeHandler}: {playerStatus: string; text: string, isSelected: boolean, style: string, onChangeHandler: () => void}) => {
+interface CustomRadioButtonProps {
+  playerStatus: string
+  text: string
+  isSelected: boolean
+  style: string
+  onChangeHandler: () => void
+}
+
+
+const CustomRadioButton = ({playerStatus, text, isSelected, style, onChangeHandler}: CustomRadioButtonProps) => {
   return (
-    <div className="radio-button-container" id={isSelected ? `${ style }` : ''} onClick={onChangeHandler}>
-      <div className="radio-slot-circle">
-        {isSelected && <div className="radio-circle" id={style}></div>}
-      </div>
-      <div className="radio-text-container" >
-        <span style={{whiteSpace: 'nowrap'}}><b>{playerStatus}</b></span>
+    <div className="radio-button-container" id={isSelected ? `${style}` : ''} onClick={onChangeHandler}>
+      <div className="radio-slot-circle">{isSelected && <div className="radio-circle" id={style}></div>}</div>
+      <div className="radio-text-container">
+        <span style={{whiteSpace: 'nowrap'}}>
+          <b>{playerStatus}</b>
+        </span>
         <span className="text" style={{color: `${isSelected ? '#ffffff' : '#7c7c84'}`}}> {text} </span>
       </div>
     </div>
