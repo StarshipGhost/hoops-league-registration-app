fcc2140ae13ff2980633059cc9fa183f2943672d
Saad Kalyati <saad.kalyati.1@ens.etsmtl.ca>
2026-02-23 06:13:44 -0500

    Change icon width in order to adapt to the box-sizing property applied to all HTML tags

--- src/components/icons/header/LockIcon.tsx
+++ src/components/icons/header/LockIcon.tsx
@@ -7,9 +7,9 @@ const LockIcon = () => {
   } = useHeaderContext()
   
   return darkMode ? (
-    <img src="./src/assets/lock_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.svg" width="18" height="18" className="header-icon" onClick={toggleAuthModal}></img>
+    <img src="./src/assets/lock_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.svg" width="34" height="34" className="header-icon" onClick={toggleAuthModal}></img>
   ) : (
-    <img src="./src/assets/lock_24dp_000000_FILL0_wght400_GRAD0_opsz24.svg" width="18" height="18" className="header-icon" onClick={toggleAuthModal}></img>
+    <img src="./src/assets/lock_24dp_000000_FILL0_wght400_GRAD0_opsz24.svg" width="34" height="34" className="header-icon" onClick={toggleAuthModal}></img>
   )
 }
 
