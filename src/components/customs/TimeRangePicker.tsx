f671a6cc0527a6de74ce25b016bd0eea605f82cd
Saad Kalyati <saad.kalyati.1@ens.etsmtl.ca>
2026-03-09 08:05:07 -0400

    Add dark theme update on open events

--- src/components/customs/TimeRangePicker.tsx
+++ src/components/customs/TimeRangePicker.tsx
@@ -1,11 +1,24 @@
 import { useEffect, useRef } from "react";
 import { PluginRegistry, TimepickerUI } from "timepicker-ui";
 import "timepicker-ui/main.css";
+import "timepicker-ui/theme-dark.css";
 import { RangePlugin } from "timepicker-ui/plugins/range";
 
 PluginRegistry.register(RangePlugin);
 
-const TimeRangePicker = ({ start, handleStartTimeUpdate, end, handleEndTimeUpdate }: { start: string; handleStartTimeUpdate: (data: string) => void; end: string; handleEndTimeUpdate: (data: string) => void }) => {
+const TimeRangePicker = ({
+  start,
+  handleStartTimeUpdate,
+  end,
+  handleEndTimeUpdate,
+  darkMode,
+}: {
+  start: string;
+  handleStartTimeUpdate: (data: string) => void;
+  end: string;
+  handleEndTimeUpdate: (data: string) => void;
+  darkMode: boolean;
+}) => {
   const inputRef = useRef<HTMLInputElement>(null);
   const pickerRef = useRef<TimepickerUI | null>(null);
 
@@ -15,38 +28,46 @@ const TimeRangePicker = ({ start, handleStartTimeUpdate, end, handleEndTimeUpdat
       ui: {
         theme: "basic",
         animation: true,
+        mobile: false,
+        editable: false,
         backdrop: true,
       },
       clock: {
         type: "12h",
-        disabledTime: { interval: "12:00AM - 8:00AM" },
+        incrementMinutes: 5,
       },
       range: {
         enabled: true,
         fromLabel: "Start",
         toLabel: "End",
-        maxDuration: 180,
       },
       callbacks: {
         onRangeConfirm: (data) => {
           handleStartTimeUpdate(data.from);
           handleEndTimeUpdate(data.to);
         },
+        onOpen: () => {
+          if (pickerRef.current && darkMode) {
+            pickerRef.current.update({
+              options: { ui: { theme: "dark" } },
+              create: true,
+            });
+          }
+        },
       },
     });
 
     pickerRef.current.create();
-    
     return () => {
       pickerRef.current?.destroy();
     };
-  }, []);
+  }, [darkMode]);
 
   const value = start.length === 0 && end.length === 0 ? "" : `${start} - ${end}`;
   return (
     <input
       ref={inputRef}
-      className="w-full text-sm dark:text-white border border-solid border-neutral-300 dark:border-neutral-800 px-3 py-2 rounded-md focus:outline-none focus:border-none focus:ring-2 focus:ring-orange-500/90 dark:focus:ring-orange-400 invalid:border-red-500 invalid:outline-red-500"
+      className="w-full text-sm dark:text-white border border-solid border-neutral-300 dark:border-input hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:hover:bg-input/50 px-3 py-2 rounded-md focus:outline-none focus:border-none focus:ring-2 focus:ring-orange-500/90 dark:focus:ring-orange-400 invalid:border-red-500 invalid:outline-red-500"
       type="text"
       value={value}
       placeholder="Select a time"
