bd9f5a419d11c2c68bd7f95ca93c839886c53d40
Saad Kalyati <saad.kalyati.1@ens.etsmtl.ca>
2026-03-09 07:28:13 -0400

    Create button variants

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,26 @@
-const Button = ({extra, text, onClick}: {extra: string; text: string; onClick?: () => void}) => {
-  return (
-    <button
-      className={`text-sm font-medium border border-solid border-neutral-200 rounded-lg whitespace-nowrap cursor-pointer hover:brightness-90 dark:border-neutral-800 ${extra}`}
-      onClick={onClick}
-    >
-      {text}
-    </button>
-  )
+interface ButtonProps {
+  extra: string,
+  text: string,
+  type?: 'submit' | 'reset' | 'button' | undefined,
+  disabled?: boolean | undefined
+  onClick?: () => void
 }
 
-export default Button
+const commonStyle = 'text-sm font-medium border border-solid border-neutral-200 dark:border-neutral-800 rounded-md whitespace-nowrap cursor-pointer transition duration-100 ease hover:brightness-90'
+export const Button = ({extra, text, type, disabled, onClick} : ButtonProps) => {
+  return <button className={`${commonStyle} ${extra}`} onClick={onClick} type={type} disabled={disabled}>{text}</button>
+}
+
+export const OrangeButton = ({ extra, text, type, onClick }: ButtonProps) => {
+  return (
+    <button className={`${commonStyle} text-white bg-orange-500/90 dark:bg-orange-500 ${extra}`} onClick={onClick} type={type}>
+      {text}
+    </button>
+  );
+};
+
+export const WhiteButton = ( { extra, text, type, onClick }: ButtonProps ) => {
+  return (
+    <button className={ `${commonStyle} text-black bg-white shadow-md ${extra}` } onClick={onClick} type={type} >{text}</button>
+  )
+}
