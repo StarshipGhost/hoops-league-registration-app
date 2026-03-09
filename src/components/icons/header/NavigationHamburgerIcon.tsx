ac506cf6917971b6e55a63d2162ad5b4f9c77a8c
Saad Kalyati <saad.kalyati.1@ens.etsmtl.ca>
2026-03-09 08:16:10 -0400

    Add selectable links to the header icons

--- src/components/icons/header/NavigationHamburgerIcon.tsx
+++ src/components/icons/header/NavigationHamburgerIcon.tsx
@@ -1,28 +1,50 @@
 import { useHeaderContext } from "../../customs/HeaderContext";
 
-const NavigationHamburgerIcon = ({open, handler}: {open: boolean; handler: () => void}) => {
-  const {theme : {darkMode}} = useHeaderContext()
+const NavigationHamburgerIcon = ({ open, handler }: { open: boolean; handler: () => void }) => {
+  const {
+    theme: { darkMode },
+  } = useHeaderContext();
   if (darkMode) {
     if (open) {
       return (
-        <img src={'./src/assets/close_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.svg'} onClick={handler} className="block w-8 h-8 cursor-pointer hover:bg-zinc-100 dark:hover:bg-zinc-800 rounded-lg" ></img>
-      )
+        <a onClick={handler}>
+          <img
+            src={"./src/assets/close_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.svg"}
+            className="block w-8 h-8 cursor-pointer hover:bg-zinc-100 dark:hover:bg-zinc-800 rounded-full"
+          ></img>
+        </a>
+      );
     } else {
       return (
-        <img src={'./src/assets/menu_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.svg'} onClick={handler} className="block w-8 h-8 cursor-pointer hover:bg-zinc-100 dark:hover:bg-zinc-800 rounded-lg" ></img>
-      )
+        <a onClick={handler}>
+          <img
+            src={"./src/assets/menu_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.svg"}
+            className="block w-8 h-8 cursor-pointer hover:bg-zinc-100 dark:hover:bg-zinc-800 rounded-full"
+          ></img>
+        </a>
+      );
     }
   } else {
     if (open) {
       return (
-        <img src={'./src/assets/close_24dp_1F1F1F_FILL0_wght400_GRAD0_opsz24.svg'} onClick={handler} className="block w-8 h-8 cursor-pointer hover:bg-zinc-100 rounded-lg" ></img>
-      )
+        <a onClick={handler}>
+          <img
+            src={"./src/assets/close_24dp_1F1F1F_FILL0_wght400_GRAD0_opsz24.svg"}
+            className="block w-8 h-8 cursor-pointer hover:bg-zinc-100 rounded-full"
+          ></img>
+        </a>
+      );
     } else {
       return (
-        <img src={'./src/assets/menu_24dp_1F1F1F_FILL0_wght400_GRAD0_opsz24.svg'} onClick={handler} className="block w-8 h-8 cursor-pointer hover:bg-zinc-100 rounded-lg" ></img>
-      )
+        <a onClick={handler}>
+          <img
+            src={"./src/assets/menu_24dp_1F1F1F_FILL0_wght400_GRAD0_opsz24.svg"}
+            className="block w-8 h-8 cursor-pointer hover:bg-zinc-100 rounded-full"
+          ></img>
+        </a>
+      );
     }
   }
-}
+};
 
 export default NavigationHamburgerIcon;
