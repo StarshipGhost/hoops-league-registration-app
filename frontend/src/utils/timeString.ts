57f21a6b96326225a76ab7c003863fb998f8b446
Saad Kalyati <saad.kalyati.1@ens.etsmtl.ca>
2026-03-19 15:24:51 -0400

    Change time format on registration date formatting function

--- frontend/src/utils/timeString.ts
+++ frontend/src/utils/timeString.ts
@@ -34,5 +34,5 @@ export const registrationTimeFormat = (date : Date) : string => {
     const year = date.getFullYear();
     const month = (date.getMonth() + 1).toString().padStart(2, '0');
     const day = date.getDate().toString().padStart(2, '0');
-    return `${year}-${month}-${day} at ${date.getHours()}:${date.getMinutes()}`
+    return `${year}-${month}-${day} at ${timeString(date)}`
 }
