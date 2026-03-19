79cb731706f7321fc5cf68fb8ef7a0a2b27a11a7
Saad Kalyati <saad.kalyati.1@ens.etsmtl.ca>
2026-03-19 15:25:24 -0400

    Make game event parameter optional

--- frontend/src/utils/scheduleUtils.ts
+++ frontend/src/utils/scheduleUtils.ts
@@ -1,6 +1,6 @@
 import type { GameEvent } from "@/types/GameEvent";
 
-export const isGameAvailable = (gameEvent: GameEvent) => {
+export const isGameAvailable = (gameEvent: GameEvent | undefined) => {
   if (gameEvent) {
     const players = gameEvent.registeredPlayers?.length;
     return players < gameEvent.capacity;
