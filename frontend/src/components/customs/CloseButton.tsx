569f83dd606e0f3c7879d7a2fea8c28f8f48f69b
Saad Kalyati <saad.kalyati.1@ens.etsmtl.ca>
2026-03-28 10:10:24 -0400

    Change icon position based on container's width

--- frontend/src/components/customs/CloseButton.tsx
+++ frontend/src/components/customs/CloseButton.tsx
@@ -1,7 +1,7 @@
 const CloseButton = ({ closeFunction }: { closeFunction: () => void }) => {
   return (
     <button
-      className="absolute -top-4 left-11/12 font-black text-3xl text-zinc-500 dark:text-zinc-400 hover:text-black dark:hover:text-white cursor-pointer"
+      className="absolute -top-5 left-9/10 sm:left-11/12 font-black text-3xl text-zinc-500 dark:text-zinc-400 hover:text-black dark:hover:text-white cursor-pointer"
       type="button"
       aria-label="Close"
       onClick={closeFunction}
