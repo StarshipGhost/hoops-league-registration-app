14b506442fd2184bd8bb112ad932573c70ead549
Saad Kalyati <saad.kalyati.1@ens.etsmtl.ca>
2026-03-28 10:17:14 -0400

    Remove unnecessary optional chaining and add explicit number type

--- frontend/src/utils/scheduleUtils.ts
+++ frontend/src/utils/scheduleUtils.ts
@@ -2,7 +2,7 @@ import type { GameEvent } from "@/types/GameEvent";
 
 export const isGameAvailable = (gameEvent: GameEvent | undefined) => {
   if (gameEvent) {
-    const players = gameEvent.registeredPlayers?.length;
+    const players: number = gameEvent.registeredPlayers.length;
     return players < gameEvent.capacity;
   } else {
     return false;
