bcd742765453f43ed9333aa6b0f9c55ef47aeec6
Saad Kalyati <saad.kalyati.1@ens.etsmtl.ca>
2026-03-28 09:57:22 -0400

    Fix 'location' dropdown menu placeholder format

--- frontend/src/components/ui/custom/Select.tsx
+++ frontend/src/components/ui/custom/Select.tsx
@@ -14,9 +14,9 @@ interface DropdownFormProps <T extends Location> {
 export function DropdownSelect<T extends Location> ({label, options, option, setCurrentOption}: DropdownFormProps<T>) {
   return (
     <Select value={option?.name ?? ""} onValueChange={(selected: string) => setCurrentOption(options.find((option) => option.name === selected))} >
-      <Label>{label}</Label>
+      <Label>{`${ label }:`}</Label>
       <SelectTrigger className="w-full dark:text-white dark:data-placeholder:text-muted-foreground hover:bg-accent dark:bg-input/30 dark:border-input dark:hover:bg-input/50 focus:border-none focus:ring-2 focus:ring-orange-500/90 dark:focus:ring-orange-400 cursor-pointer">
-        <SelectValue  className='' defaultValue={`Select a ${label}`} placeholder={ `Select a ${ label }` }>
+        <SelectValue  className='' defaultValue={`Select a ${label.toLowerCase()}`} placeholder={ `Select a ${ label.toLowerCase() }` }>
         </SelectValue> 
       </SelectTrigger>
       <SelectContent className="bg-white dark:bg-black z-9999" position='popper'>
