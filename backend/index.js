a0355c8d0e16fac0d509c0b10a1d4a28c5423ecc
Saad Kalyati <saad.kalyati.1@ens.etsmtl.ca>
2026-04-08 08:03:45 -0400

    Fully migrate from in-memory server data to database

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,25 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const corsOptions = require("./config/corsOptions");
-const { logger } = require("./middleware/logger");
+const { logger, logEvents } = require("./middleware/logger");
 const { errorHandler } = require("./middleware/errorHandler");
 const { setCookie } = require("./middleware/setCookie");
 const cookieParser = require("cookie-parser");
-const { requireAdmin } = require('./middleware/requireAdmin')
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const PORT = 3000;
+const { requireAdmin } = require("./middleware/requireAdmin");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const connectDB = require("./config/dbConnect");
+const mongoose = require("mongoose");
+const GameEvent = require("./models/GameEvent");
 
 app.use(cors(corsOptions));
 app.use(logger);
 app.use(express.json());
 app.use(express.static("dist"));
 app.use(cookieParser());
-
-let schedule = [];
+connectDB();
 
 app.use(setCookie);
 
@@ -26,55 +27,68 @@ app.get("/guest", (req, res) => {
   return res.status(200).json({ guestId: req.guestId });
 });
 
-app.get("/schedule", (req, res) => {
+app.get("/schedule", async (req, res, next) => {
+  const schedule = await GameEvent.find({});
   return res.status(200).json(schedule);
 });
 
-app.get("/schedule/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const game = schedule.find((g) => g.id === id);
+app.get("/schedule/:id", async (req, res, next) => {
+  const id = req.params.id;
+  const game = await GameEvent.findById({ _id: id });
   if (game) {
     return res.status(200).json(game);
   }
   return res.status(404).json({ error: "game not found" }).end();
 });
 
-app.patch("/schedule/:id", (req, res) => {
+app.patch("/schedule/:id", async (req, res) => {
   const isEmpty = (data) => !data.length;
   const { date, start, end, location, capacity, openRegistrations } = req.body;
-  const id = parseInt(req.params.id);
-  const game = schedule.find((g) => g.id === id);
+  const id = req.params.id;
+  const game = await GameEvent.findById({ _id: id });
+  if (!game) {
+    return res.status(404).json({ error: "game not found" });
+  }
   if (isEmpty(date) || isEmpty(start) || isEmpty(end) || !location || capacity < game.registeredPlayers) {
     return res.status(400).json({ error: "All fields are required" });
   }
-  const updatedGame = { ...game, date: date, start: start, end: end, location: location, capacity: capacity, openRegistrations: openRegistrations };
-  schedule = schedule.map((game) => (game.id === id ? updatedGame : game));
+  game.date = date;
+  game.start = start;
+  game.end = end;
+  game.location = location;
+  game.capacity = capacity;
+  game.openRegistrations = openRegistrations;
+
+  const updatedGame = await game.save();
   return res.status(201).json(updatedGame);
 });
 
-app.post("/schedule", (req, res) => {
+app.post("/schedule", async (req, res) => {
   const isEmpty = (data) => !data.length;
   const game = req.body;
   const newId = schedule.length + 1;
-  if (isEmpty(game.date) || isEmpty(game.start) || isEmpty(game.end) || !game.location) {
+  if (isEmpty(game.date) || isEmpty(game.start) || isEmpty(game.end) || !game.location || isNaN(capacity)) {
     return res.status(400).json({ error: "All fields are required" });
   }
-  const newGame = { ...game, id: newId, registeredPlayers: [] };
-  schedule = schedule.concat(newGame);
-  return res.status(201).json(newGame).end();
+  const gameObject = { ...game, id: newId, registeredPlayers: [] };
+  const newGame = await GameEvent.create(gameObject);
+  if (newGame) {
+    return res.status(201).json(newGame).end();
+  }
+  return res.status(400).json({ message: "Invalid game data received" });
 });
 
-app.delete("/schedule/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  schedule = schedule.filter((game) => game.id !== id);
-  return res.status(200).json(schedule).end();
+app.delete("/schedule/:id", async (req, res) => {
+  const id = req.params.id;
+  const game = await GameEvent.findByIdAndDelete({ _id: id });
+  return res.status(200).json({ message: `${game} with ID ${game.id} deleted` });
 });
 
-app.post("/schedule/:id/register", (req, res) => {
-  const id = parseInt(req.params.id);
-  const game = schedule.find((g) => g.id === id);
+app.post("/schedule/:id/register", async (req, res) => {
+  const id = req.params.id;
+  const game = await GameEvent.findById({ _id: id });
   if (!game) {
-    return res.status(404).json({ error: "Game not found" });
+    return res.status(404).json({ error: "game not found" });
   }
   const { firstName, status, registrationTime } = req.body;
   if (!firstName || !status) {
@@ -82,37 +96,40 @@ app.post("/schedule/:id/register", (req, res) => {
   }
   const alreadyRegistered = game.registeredPlayers.some((player) => player.guestId === req.guestId);
   if (alreadyRegistered) {
-    return res.status(409).json({ error: "This client is already registered for this game" });
+    return res.status(409).json({ error: "this client is already registered for this game" });
   }
   if (game.registeredPlayers.length >= game.capacity) {
-    return res.status(400).json({ error: "This game is already full" });
+    return res.status(400).json({ error: "this game is already full" });
   }
   const newRegisteredPlayer = {
     guestId: req.guestId,
     firstName,
     status,
-    registrationTime
+    registrationTime,
   };
-  const updatedGame = { ...game, registeredPlayers: [...game.registeredPlayers, newRegisteredPlayer] };
-  schedule = schedule.map((g) => (g.id === id ? updatedGame : g));
+
+  game.registeredPlayers.push(newRegisteredPlayer);
+  await game.save();
   return res.status(200).json(newRegisteredPlayer).end();
 });
 
-app.delete("/schedule/:id/register/:playerId", (req, res) => {
-  const id = parseInt(req.params.id);
+app.delete("/schedule/:id/register/:playerId", async (req, res) => {
+  const id = req.params.id;
   const playerId = req.params.playerId;
-  const game = schedule.find((g) => g.id === id);
+  const game = await GameEvent.findById({ _id: id });
   if (!game) {
     return res.status(404).json({ error: "Game not found" });
   }
-  const updatedGame = {...game, registeredPlayers: game.registeredPlayers.filter((player) => player.guestId !== playerId)}
-  schedule = schedule.map((game) => game.id === id ? updatedGame : game);
-  return res.status(201).end()
-})
+
+  const playerIndex = game.registeredPlayers.findIndex((player) => player.guestId === playerId);
+  game.registeredPlayers.splice(playerIndex, 1);
+
+  await game.save();
+  return res.status(201).json(updatedGame).end();
+});
 
 app.post("/admin/login", async (req, res) => {
   const { username, password } = req.body;
-  console.log(req.body)
 
   if (username !== process.env.ADMIN_EMAIL) {
     return res.status(401).json({ error: "Invalid credentials" });
@@ -121,27 +138,15 @@ app.post("/admin/login", async (req, res) => {
   if (!isValid) {
     return res.status(401).json({ error: "Invalid credentials" });
   }
-  const token = jwt.sign(
-    { role: "admin" },
-    process.env.JWT_SECRET,
-    { expiresIn: "15m" }
-  );
+  const token = jwt.sign({ role: "admin" }, process.env.JWT_SECRET, { expiresIn: "15m" });
 
   res.cookie("admin_token", token, {
       httpOnly: true,
       secure: false, // true in production with HTTPS
       sameSite: "lax",
-    }).status(200).json({ message: "Logged in successfully" });
-});
-
-app.post("/admin/logout", (req, res) => {
-  res.clearCookie("admin_token", {
-    httpOnly: true,
-    secure: false, // true in production with HTTPS
-    sameSite: "lax",
-  });
-
-  res.status(200).json({ message: "Logged out successfully" });
+    })
+    .status(200)
+    .json({ message: "Logged in successfully" });
 });
 
 app.post("/admin/logout", (req, res) => {
@@ -159,4 +164,13 @@ app.get("/admin/me", requireAdmin, (req, res) => {
 });
 
 app.use(errorHandler);
-app.listen(PORT, () => console.log(`server listens to port ${PORT}`));
+
+mongoose.connection.once("open", () => {
+  app.listen(process.env.PORT, () => console.log(`server listens to port ${process.env.PORT}`));
+  console.log("Connected to MongoDB");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+  logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, "mongoErrLog.log");
+});
---
+++ backend/models/GameEvent.js
@@ -0,0 +1,63 @@
+const mongoose = require("mongoose");
+
+const gameEventSchema = new mongoose.Schema({
+  date: {
+    type: String,
+    required: true,
+  },
+  start: {
+    type: String,
+    required: true,
+  },
+  end: {
+    type: String,
+    required: true,
+  },
+  location: {
+    name: {
+      type: String,
+      required: true,
+    },
+    link: {
+      type: String,
+      required: true,
+    },
+  },
+  registeredPlayers: [
+    {
+      guestId: {
+        type: String,
+        requred: true,
+      },
+      firstName: {
+        type: String,
+        required: true,
+      },
+      status: {
+        type: String,
+        required: true,
+      },
+      registrationTime: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  capacity: {
+    type: Number,
+    required: true,
+  },
+  openRegistrations: {
+    type: Boolean,
+  },
+});
+
+gameEventSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model("GameEvent", gameEventSchema);
