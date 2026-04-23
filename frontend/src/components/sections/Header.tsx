1e3c7f829e2a4d8d88e181e36b19d21952bc1461
Saad Kalyati <saad.kalyati.1@ens.etsmtl.ca>
2026-04-23 04:53:27 -0400

    Add outside click handling for expanded header using useRef

--- frontend/src/components/sections/Header.tsx
+++ frontend/src/components/sections/Header.tsx
@@ -1,17 +1,17 @@
-import {useState} from 'react'
+import {useRef, useState, type RefObject} from 'react'
 import {BasketballLogo} from '../icons/BasketballLogo'
 import {useHeaderContext} from '../customs/HeaderContext'
 import NavigationHamburgerIcon from '../icons/header/NavigationHamburgerIcon'
 import ColorToggleIcon from '../icons/header/ColorToggleIcon'
 import LockIcon from '../icons/header/LockIcon'
 import { OrangeButton } from '../customs/Button'
+import { useOutsideClick } from '@/utils/outsideClick'
 
 const navigationLinkStyle: {desktop: string; mobile: string} = {
   desktop: 'text-sm text-zinc-500 dark:text-zinc-400 font-medium p-2 white-space-nowrap cursor-pointer hover:text-black dark:hover:text-white',
   mobile: 'max-w-full text-sm text-zinc-500 dark:text-zinc-400 font-medium p-2 whitespace-nowrap cursor-pointer hover:bg-zinc-100 dark:hover:bg-zinc-800 rounded-md',
 }
 const links = ['Schedule', 'Registered Players', 'Rules', 'Pricing', 'Register']
-
 
 const HeaderLink = ({link, style, scrollFunction}: {link: string; style: string; scrollFunction: () => void}) => {
   return <li onClick={scrollFunction} className={style}> <a>{link}</a> </li>
@@ -22,10 +22,16 @@ const DesktopNavigationBar = () => {
   const {desktop} = navigationLinkStyle
   return (
     <ul className="hidden lg:flex items-center gap-4">
-      {links.map((link, index) => ( <HeaderLink key={link} link={link} style={desktop} scrollFunction={() => scrollFunction(index + 1)} />))}
+      {links.map((link, index) => 
+          <HeaderLink 
+              key={link} 
+              link={link} 
+              style={desktop} 
+              scrollFunction={() => scrollFunction(index + 1)} />
+      )}
       <li><ColorToggleIcon /></li>
       <li><LockIcon open={isAdmin}/></li>
-      <li> <OrangeButton extra="px-4 py-2" text="Join Now" onClick={() => scrollFunction(5)} /></li>
+      <li><OrangeButton extra="px-4 py-2" text="Join Now" onClick={() => scrollFunction(5)} /></li>
     </ul>
   )
 }
@@ -33,10 +39,20 @@ const DesktopNavigationBar = () => {
 const MobileNavigationBar = ({open, closeNavBarFunction}: {open: boolean; closeNavBarFunction: () => void}) => {
   const {scrollFunction} = useHeaderContext()
   const {mobile} = navigationLinkStyle
+  const navigate = (index: number) => {
+    scrollFunction(index);
+    closeNavBarFunction();
+  }
   return (
     <ul className={`w-full bg-white dark:bg-black flex flex-col gap-2 p-4 absolute z-1 transition duration-200 ease-linear delay-100 -translate-y-full border-box lg:hidden ${open && `translate-y-0`}`} >
-      {links.map((link, index) => ( <HeaderLink key={link} link={link} style={mobile} scrollFunction={() => { scrollFunction(index + 1); closeNavBarFunction() }} />))}
-      <li><OrangeButton extra="w-full px-4 py-2" text="Join Now" onClick={() => { scrollFunction(5); closeNavBarFunction() }} /></li>
+      {links.map((link, index) => 
+          <HeaderLink 
+              key={link} 
+              link={link} 
+              style={mobile} 
+              scrollFunction={() => navigate(index + 1)} />
+            )}
+      <li><OrangeButton extra="w-full px-4 py-2" text="Join Now" onClick={() => navigate(5)}/></li>
     </ul>
   )
 }
@@ -44,8 +60,10 @@ const MobileNavigationBar = ({open, closeNavBarFunction}: {open: boolean; closeN
 const Header = () => {
   const [open, setOpen] = useState<boolean>(false)
   const {admin: {isAdmin}} = useHeaderContext();
+  const header : RefObject<HTMLElement | null> = useRef<HTMLElement | null>(null)
+  useOutsideClick(header, open, () => setOpen(false))
   return (
-    <div className="sticky top-0 z-3">
+    <header ref={header} className="sticky top-0 z-3">
       <div className="bg-white dark:bg-black flex items-center justify-between border-b border-solid border-neutral-200 dark:border-neutral-500 sm:dark:border-neutral-800 border-box  pr-5 pb-4 pl-4 lg:pl-8 pt-4 relative z-4">
         <BasketballLogo />
         <DesktopNavigationBar />
@@ -56,7 +74,7 @@ const Header = () => {
         </div>
       </div>
       <MobileNavigationBar open={open} closeNavBarFunction={() => setOpen((v) => !v)} />
-    </div>
+    </header>
   )
 }
 
---
+++ frontend/src/utils/outsideClick.ts
@@ -0,0 +1,17 @@
+import { useEffect, type RefObject } from "react";
+
+export function useOutsideClick (ref: RefObject<HTMLElement | null>, open : boolean, handler: () => void) {
+    useEffect(() => {
+        const nodeRef = ref.current 
+        function handleOutsideClick (event: Event)  {
+            if (open && nodeRef && !nodeRef.contains(event.target as Node)) {
+                handler();
+                console.log("hello")
+            }
+        }
+        window.addEventListener('click', handleOutsideClick);
+        return () => {
+            window.removeEventListener('click', handleOutsideClick);
+        }
+    }, [ref, open, handler])
+}
